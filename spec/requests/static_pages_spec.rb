ee889c0bd9191524c745e60c1f678e38e5af955b
semplice1979 <semplice.tw@yahoo.com.tw>
2015-08-03 17:43:13 +0800

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,6 +35,38 @@ describe "Static pages" do
 		it { should have_title (full_title('Contact'))}
 	end
 
+
+
+	describe "full_title" do
+   		it "should include the page title" do
+      		expect(full_title("foo")).to match(/foo/)
+ 		end
+ 	   	it "should include the base title" do
+      		expect(full_title("foo")).to match(/^Ruby on Rails Tutorial Sample App/)
+		end
+
+		it "should not include a bar for the home page" do
+      		expect(full_title("")).not_to match(/\|/)
+		end 
+	end
+
+=begin
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		expect(page).to have_title(full_title('About Us')) 
+		click_link "Help"
+		expect(page).to # fill in
+		click_link "Contact"
+		expect(page).to # fill in
+		click_link "Home"
+		click_link "Sign up now!"
+		expect(page).to # fill in
+		click_link "sample app"
+		expect(page).to # fill in
+	end
+=end
+
 end
 
 =begin
